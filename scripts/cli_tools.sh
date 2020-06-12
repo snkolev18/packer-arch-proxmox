059c9f14b32bc1f19b33c28fc6fe77c8cfaff55e
Lee <Themodem@users.noreply.github.com>
2020-06-12 20:20:30 +0100

    Update cli_tools.sh

--- scripts/cli_tools.sh
+++ scripts/cli_tools.sh
@@ -4,13 +4,4 @@ set -e
 set -x
 
 # Basic tooling
-sudo pacman -S --noconfirm vim git wget ranger
-
-# Install trizen (AUR helper)
-mkdir -p /home/dev/git && cd $_
-git clone https://aur.archlinux.org/trizen.git
-cd trizen
-makepkg --noconfirm -si
-
-# install terminal toys
-trizen -S --noconfirm asciiquarium fortune-mod lolcat cowsay asciiquarium tty-clock cmatrix neofetch htop
+sudo pacman -S --noconfirm vim git wget 
